9f01ded32d5b761c3d6906e88de2d1fc2e0a9da0
Mayuri20071999 <Mayuri.Deshmukh@ibm.com>
2025-12-02 14:32:15 +0530

    Add files via upload

---
+++ Java/Activities/Activity4.java
@@ -0,0 +1,30 @@
+package Activities;
+
+import java.util.Arrays;
+
+public class Activity4 {
+	public static void main(String args[]) {
+		
+		int arr[]= {10,34,2,4,1};
+		int temp;
+		
+		for(int i =0; i<arr.length; i++) {
+			
+			for(int j=0; j<arr.length-1; j++) {
+				if(arr[j] > arr[j+1]) {
+					temp = arr[j]; 
+					arr[j] = arr[j+1]; 
+					arr[j+1] = temp;
+					 
+					
+					
+				}
+				
+			}
+			
+		}
+		System.out.print(Arrays.toString(arr));
+		
+	}
+
+}
